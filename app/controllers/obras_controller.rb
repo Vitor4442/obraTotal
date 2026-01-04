d4276fbe1814c7d1b1043c31ebb494cca8b000bb
José Vitor <jose.vitorw018@gmail.com>
2026-01-03 23:46:08 -0300

    Versão 1 das obras finalizada

--- app/controllers/obras_controller.rb
+++ app/controllers/obras_controller.rb
@@ -1,13 +1,13 @@
 class ObrasController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_obra, only: %i[show edit update]
+  before_action :set_obra, only: %i[show edit update destroy]
 
   def index
     @obras = load_obras.ordered
   end
 
-  def show; 
-end
+  def show
+  end
 
   def new
     @obra = Obra.new
@@ -16,40 +16,39 @@ end
   def create
     @obra = Obra.new(obra_params)
     @obra.created_by_id = current_user.id
+    @obra.created_at = Time.current
     @obra.progresso = 0
-  if @obra.save
-    redirect_to obras_path, notice: "Obra criada!", status: :see_other
-else
-    render :new, status: :unprocessable_entity
-  end
-  end
 
-  def edit; end
-
-  def update
-    @obra.updated_by = current_user.id
-
-    if @obra.update(obra_params)
-      redirect_to @obra
+    if @obra.save
+      redirect_to obras_path, notice: "Obra criada!", status: :see_other
     else
-      render :edit
+      render :new, status: :unprocessable_entity
     end
   end
 
- def destroy
-  @obra = Obra.find(params[:id])
+  def edit
+  end
 
-  @obra.update!(
-    deleted_at: Time.current,
-    deleted_by_id: current_user.id
-  )
+  def update
+  
+    @obra.updated_by_id = current_user.id 
+    @obra.updated_at = Time.current
+    
+    if @obra.update(obra_params)
+      redirect_to obras_path, notice: "Obra Atualizada!", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
-  redirect_to obras_path,
-              notice: "Obra excluída com sucesso!",
-              status: :see_other
-end
+  def destroy
+    @obra.update!(
+      deleted_at: Time.current,
+      deleted_by_id: current_user.id
+    )
 
-
+    redirect_to obras_path, notice: "Obra excluída com sucesso!", status: :see_other
+  end
 
   private
 
@@ -58,10 +57,10 @@ end
   end
 
   def obra_params
-    params.require(:obra).permit(:nome, :endereco, :progresso)
+    params.require(:obra).permit(:nome, :endereco)
   end
 
   def load_obras
-    @obras = Obra.where(created_by_id: current_user, deleted_at: nil)
+    Obra.where(created_by_id: current_user, deleted_at: nil)
   end
 end
--- app/models/obra.rb
+++ app/models/obra.rb
@@ -1,6 +1,6 @@
 class Obra < ApplicationRecord
-    validates :nome, presence: true
-    validates :endereco, presence: true
+validates :nome, presence: { message: "O nome do projeto não pode ficar em branco" }
+  validates :endereco, presence: { message: "A localização é obrigatória para cadastrar a obra" }
 
     scope :active, -> { where(deleted_at: nil) }
     scope :by_user, ->(user) { where(created_by_id: user.id) }
